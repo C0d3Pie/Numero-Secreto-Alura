7856831e87bba01cd0d31959f4168f611a4d6a9d
Xavico Kzan <xavicokzan@gmail.com>
2024-05-14 18:00:51 -0300

    Primeiro Projeto de JS na Alura

---
+++ app.js
@@ -0,0 +1,43 @@
+alert ("Boas vindas ao jogo do Número Secreto!");
+let numeroMaximo = 100
+let numeroSecreto = parseInt (Math.random() * numeroMaximo + 1);
+console.log ("numeroSecreto");
+let chute;
+let tentativas = 1;
+
+//Enquanto o chute não for igual o número secreto
+while (chute != numeroSecreto)
+    {
+        chute = prompt (`Escolha um número entre 1 e ${numeroMaximo}`);
+     //Se chute for igual ao número secreto
+        if (chute == numeroSecreto)
+            {
+            break;
+            }
+        else
+            {
+            if (chute > numeroSecreto)
+                {
+                alert (`O número secreto é menor que ${chute}`);
+                }
+            else
+                {
+                alert (`O número secreto é maior que ${chute}`);
+                }
+                //tentativas = tentativas + 1;
+                tentativas++;
+            }   
+    }
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert (`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa} :)`);
+
+/*
+if (tentativas > 1)
+    {
+        alert (`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas :)`);
+    }
+else
+    {
+        alert (`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa :)`);
+    }
+*/
